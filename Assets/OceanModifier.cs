54765c21b70aedc146972e7ee9569f55a1ee2454
nycucumber <yang.wang.dec@gmail.com>
2015-06-16 12:02:06 -0400

    initilized OCEAN and UNIDUINO

---
+++ Assets/OceanModifier.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OceanModifier : MonoBehaviour {
+
+	public float oceanScale = 0.0f;
+	public Ocean myOcean;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myOcean.scale = oceanScale;
+	}
+}
