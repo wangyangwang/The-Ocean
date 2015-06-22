f14be3788bfa05cd208aaf66ff4cc1c13e00c064
nycucumber <yang.wang.dec@gmail.com>
2015-06-21 21:48:33 -0400

    ocean variables ready

--- Assets/OceanModifier.cs
+++ Assets/OceanModifier.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class OceanModifier : MonoBehaviour {
 
-	public float oceanScale = 0.0f;
+	public float oceanScale = 0.0f;//1-10
+	public float waveSpeed = 0.6f; //0-3
+	public float windPower = 0.5f;//0-1
+
 	public Ocean myOcean;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,5 +17,8 @@ public class OceanModifier : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		myOcean.scale = oceanScale;
+//		myOcean.wa
+		myOcean.speed = waveSpeed;
+		myOcean.humidity = windPower;
 	}
 }
